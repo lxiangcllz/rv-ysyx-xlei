806cf774091ef91d4baa9540971aa40ab4c250ab
向磊 <leixiang.en@foxmail.com>
2026-07-11 20:15:11 +0800

    modify some notes about C compiling.

--- phase_E/emu/from_C_to_binary/precomp/a.c
+++ phase_E/emu/from_C_to_binary/precomp/a.c
@@ -3,10 +3,14 @@
 #define MSG "Hello \
 World!\n"
 
+#define _str(x) #x
+#define _concat(a, b) a##b
+
 int main() {
 	printf(MSG);
 #ifdef __riscv
 	printf("Hello RISC-V!\n");
 #endif
+  _concat(pr, intf)(_str(RISC-V));
 	return 0;
 }
