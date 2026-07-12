9cb45b30d8e5b4e7270c55891852740a6e75f0d6
向磊 <leixiang.en@foxmail.com>
2026-07-13 00:18:22 +0800

    enhance sEMU.c to receive a user-defined argument N and support one more self-defined instr 'out r2' to print the sum to terminal.

--- phase_E/emu/instr_emu/sEMU_freestanding/sEMU.c
+++ phase_E/emu/instr_emu/sEMU_freestanding/sEMU.c
@@ -9,29 +9,29 @@
  * | 11 |   addr   | rs2 | if (R[0]!=R[rs2]) PC=addr bner0指令, 若不等于R[0]则跳转
  * +----+----------+-----+
  *
- * An example case: evaluate 1+2+...+10
+ * An example case: evaluate 1+2+...+N (N is user input)
  * The corresponding sISA instrs:
- *   0b10001010, // li r0, 10
  *   0b10010000, // li r1, 0
  *   0b10100000, // li r2, 0
  *   0b10110001, // li r3, 1
  *   0b00010111, // add r1, r1, r3
  *   0b00101001, // add r2, r2, r1
- *   0b11010001, // bner0 r1, 4
- *   0b11011111, // bner0 r3, 7
+ *   0b11001101, // bner0 r1, 3
+ *   0b01000010, // out r2
  * The corresponding runtime environment needed:
  * 1, 在程序执行开始前
- *   1) 加载程序
- *   2) 暂时不支持程序参数的传递
+ *   1) 加载程序: 将程序从文件中读入M
+ *   2) 传递参数: 在r0寄存器中存放程序的参数
  * 2, 在程序执行过程中
- *   1) 暂时不支持库函数
+ *   1) 可通过out指令以整数方式输出指定寄存器的值
  * 3, 在程序执行结束后
- *   1) 通过死循环来指示程序结束 - 通过bner0指令实现
+ *   1) 通过自定义指令out来输出结果
  */
 
 #include <stdint.h>
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 
 uint8_t PC = 0;
 uint8_t R[4];
@@ -54,6 +54,11 @@ typedef union {
     uint8_t addr : 4;
     uint8_t op : 2;
   } bne;
+  struct {
+    uint8_t rd : 2;
+    uint8_t : 4;
+    uint8_t op : 2;
+  } out;
   uint8_t byte;
 } inst_t;
 
@@ -62,14 +67,21 @@ void inst_cycle() {
   switch ((instr.byte >> 6) & 0b11) {
     case 0b00: R[instr.r.rd] = R[instr.r.rs1] + R[instr.r.rs2]; break;
     case 0b10: R[instr.li.rd] = instr.li.imm; break;
-    case 0b11: { if (R[instr.bne.rs2] != R[0]) PC = instr.bne.addr; return; }
+    case 0b11:  
+               if (R[instr.bne.rs2] != R[0]) { 
+                 PC = instr.bne.addr;
+                 return; 
+               }
+               break;
+    case 0b01: printf("%u\n", R[2]); break;
     default: printf("Unsuppported instruction\n"); break;
   }
   PC++;
 }
 
 int main(int argc, char* argv[]) {
-  assert(argc >= 2);
+  assert(argc >= 3);
+  R[0] = atoi(argv[2]);
   FILE *fp = fopen(argv[1], "r");
   assert(fp != NULL);
   int ret = fseek(fp, 0, SEEK_END);
@@ -84,6 +96,5 @@ int main(int argc, char* argv[]) {
   for (int i = 0; i < 100; i++) {
     inst_cycle();
   }
-  printf("r2 = %d\n", R[2]); // sum of 1+2+...+10
   return 0;
 }
